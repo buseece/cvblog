f2510d6da04a48263760f6e48c9600d977019238
Buse Ece Güldiken <buseecegs@gmail.com>
2020-04-03 18:22:47 +0300

    Updates

---
+++ precache-manifest.a54c270cbed9394b75478dcca461bdb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84925cbc5375a2f6d7a71818eeaedae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52bf7cc518990335095c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5b208aef7bdb4301fff1",
+    "url": "/static/css/main.7860922f.chunk.css"
+  },
+  {
+    "revision": "52bf7cc518990335095c",
+    "url": "/static/js/2.c4eb05d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c4eb05d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b208aef7bdb4301fff1",
+    "url": "/static/js/main.eb94d207.chunk.js"
+  },
+  {
+    "revision": "905e4d6bd7d9946d806f",
+    "url": "/static/js/runtime-main.8ab15afb.js"
+  }
+]);
